3afc4667450a7d0b9c422a1c1c9e2180c5131f7d
Emmanuel <emmanue.kimweli@student.moringaschool.com>
2022-08-20 02:10:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(b){
+    if(b>42){
+        return b-42;
+    }
+    else if (b<42){
+        return 42-b;
+    }
+}
+
+function distanceFromHqInFeet(f){
+    if(f>42){
+        return (f-42)*264;
+    }
+    else if (f<42){
+        return (42-f)*264;
+    }
+}
+
+function distanceTravelledInFeet(num1,num2){
+    if(num1>num2){
+        return (num1-num2)*264;
+    }
+    else if (num2>num1){
+        return (num2-num1)*264;
+    }
+}
+
+function calculatesFarePrice(d1,d2){
+        if((d1-d2)*264 <= 400){
+          return 0;
+        }
+         else if((d1-d2)*264 < 2000){
+            return ((((d1-d2)*264)-400)*0.02);
+         }
+         else if(((d1-d2)*264) <= 2500){
+            return 25;
+         }
+         else if((d1-d2)*264 > 2500){
+          return "cannot travel that far";}
+    }
+
